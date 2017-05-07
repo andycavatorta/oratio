1bd7717309e7290ad7fd5b260b725f037e262d21
Andy Cavatorta <andycavatorta@gmail.com>
2017-05-07 09:05:29 -0400

    added stub of thirtybirds server to controller

--- Roles/avl_controller/main.py
+++ Roles/avl_controller/main.py
@@ -26,3 +26,48 @@ Dashboard:
 
 
 
+
+import time
+import threading
+import settings
+import yaml
+import json
+
+from thirtybirds_2_0.Logs.main import Exception_Collector
+from thirtybirds_2_0.Network.manager import init as network_init
+
+
+def network_status_handler(msg):
+    print "network_status_handler", msg
+
+
+def network_message_handler(msg):
+    try:
+        print "network_message_handler", msg
+        topic = msg[0]
+        #print "topic", topic 
+        if topic == "__heartbeat__":
+            print "heartbeat received", msg
+    except Exception as e:
+        print "exception in network_message_handler", e
+
+network = None
+
+def init(HOSTNAME):
+    global network
+    network = network_init(
+        hostname=HOSTNAME,
+        role="server",
+        discovery_multicastGroup=settings.discovery_multicastGroup,
+        discovery_multicastPort=settings.discovery_multicastPort,
+        discovery_responsePort=settings.discovery_responsePort,
+        pubsub_pubPort=settings.pubsub_pubPort,
+        message_callback=network_message_handler,
+        status_callback=network_status_handler
+    )
+    network.subscribe_to_topic("system")  # subscribe to all system messages
+    network.subscribe_to_topic("sensor_data")  
+    network.subscribe_to_topic("cell_data")
+    network.subscribe_to_topic("incubator_data")
+    network.subscribe_to_topic("algorithm_data")
+    
