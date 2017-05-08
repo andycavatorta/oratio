87cc15af21545dc099026e2c9b38bd791f5eea91
Andy Cavatorta <andycavatorta@gmail.com>
2017-05-07 20:58:16 -0400

    typo

--- Roles/avl_controller/main.py
+++ Roles/avl_controller/main.py
@@ -95,13 +95,13 @@ class Dispatcher(threading.Thread):
         return final_freq
 
 
-    def calculate_harmonic_pitch(self, voice_num, base_pitch):
+    def calculate_harmonic_pitch(self, voice_num, base_pitch, db_harmonic_num):
         voice = self.voices[voice_num]
-        if harmonic == 0:
+        if db_harmonic_num == 0:
             harmonic_freq = (int(voice["db_h1_harmonic"]) + 1) * base_pitch
             final_freq = harmonic_freq * pow(2, (voice["db_h1_fine"]/1200))
             return final_freq
-        if harmonic == 1:
+        if db_harmonic_num == 1:
             harmonic_freq = (int(voice["db_h2_harmonic"]) + 1) * base_pitch
             final_freq = harmonic_freq * pow(2, (voice["db_h2_fine"]/1200))
             return final_freq
