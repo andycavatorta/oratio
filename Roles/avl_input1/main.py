3a36b7fdb24af159295dfc00efdd314311b813dd
Andy Cavatorta <andycavatorta@gmail.com>
2017-05-08 20:22:13 -0400

    adding interfaces for cap sensors and pitch key encoders.

--- Roles/avl_input1/main.py
+++ Roles/avl_input1/main.py
@@ -10,10 +10,11 @@ output topics:
     voice_key_3_position - float from 0.0 to 1.0
 
 """
-
+import Adafruit_MPR121.MPR121 as MPR121
 import importlib
 import json
 import os
+import Queue
 import random
 import settings 
 import sys
@@ -35,28 +36,79 @@ class Main(threading.Thread):
     def __init__(self, hostname):
         threading.Thread.__init__(self)
         self.hostname = hostname
+        self.queue = Queue.Queue()
+
+    def add_to_queue(self, topic, msg):
+        self.queue.put([topic, msg])
 
     def run(self):
         while True:
-            network.send("pitch_key_event", random.randrange(0,47))
-            time.sleep(random.randrange(0,3))
-            network.send("voice_key_1_position", random.randrange(0,100)/100.0)
-            time.sleep(random.randrange(0,3))
-            network.send("voice_key_2_position", random.randrange(0,100)/100.0)
-            time.sleep(random.randrange(0,3))
-            network.send("voice_key_3_position", random.randrange(0,100)/100.0)
-            time.sleep(random.randrange(0,3))
-            network.send("pitch_key_event", random.randrange(0,47))
-            time.sleep(random.randrange(0,3))
-            network.send("voice_key_1_position", random.randrange(0,100)/100.0)
-            time.sleep(random.randrange(0,3))
-            network.send("voice_key_2_position", random.randrange(0,100)/100.0)
-            time.sleep(random.randrange(0,3))
-            network.send("voice_key_3_position", random.randrange(0,100)/100.0)
-            time.sleep(random.randrange(0,3))
+            topic_msg = self.queue.get(True)
+            network.send(topic_msg[0], topic_msg[1])
 
-        ###  ###
+class MPR121Array(threading.Thread):
+    def __init__(self, i2c_address):
+        threading.Thread.__init__(self)
+        position_raw = 0
+        self.i2c_address = i2c_address
+        self.capsensors = []
+        self.last_touched = [0,0,0,0]
+        
+        for sensor_id in range(4):
+            self.capsensors.append(MPR121.MPR121())
+            if not self.capsensors[sensor_id].begin(self.i2c_address[sensor_id]):
+                print('Error initializing MPR121 @{}'.format(self.i2c_address[sensor_id]))
+            print repr(self.capsensors[sensor_id])
+        print "class CapSensorArray instantiated with values", self.i2c_address
 
+    def run(self):
+        print "class CapSensorArray thread started"
+        for sensor_id in range(4):
+            self.last_touched[sensor_id] = self.capsensors[sensor_id].touched()
+            global_position = 1
+        while True:
+            for sensor_id in range(4):
+                current_touched = self.capsensors[sensor_id].touched()
+                for i in range(12):
+                    pin_bit = 1 << i
+                    if current_touched & pin_bit and not self.last_touched[sensor_id] & pin_bit:
+                        print('{0} touched!'.format(i))
+                        global_position = i + (12 * sensor_id)
+                    if not current_touched & pin_bit and self.last_touched[sensor_id] & pin_bit:
+                        print('{0} released!'.format(i))
+                self.last_touched[sensor_id] = current_touched
+            if global_position > 1:
+                time.sleep(0.01)
+                main.add_to_queue("pitch_key_event", global_position)
+
+class Key(threading.Thread):
+    def __init__(self, name, bus, deviceId):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.bus = bus
+        self.deviceId = deviceId
+        print "creating amt203 object"
+        self.encoder = AMT203.AMT203(bus, deviceId)
+        print "setting zero ", self.bus, self.deviceId
+        self.encoder.set_zero()
+        print "after zero ", self.bus, self.deviceId 
+        print "class Key instantiated with values", name, bus, deviceId
+        self.encoder_min = 0
+        self.encoder_max = 100
+
+    def run(self):
+        print "class Key thread started"
+        while True:
+            pos = self.encoder.get_position()
+            mapped_pos = self.map_key(self.name, pos)
+            main.add_to_queue(self.name, mapped_pos)
+            time.sleep(0.01)
+
+    def map_key(self, name, value):
+        value = encoder_max if value > encoder_max else value
+        value = encoder_min if value < encoder_min else value
+        mapped_value = (((value - encoder_min))/(encoder_max - encoder_min))
+        return mapped_value
 
 def network_status_handler(msg):
     print "network_status_handler", msg
@@ -87,4 +139,14 @@ def init(HOSTNAME):
     #network.subscribe_to_topic("sensor_data")  
     main = Main(HOSTNAME)
     main.start()
+    mpr121array = MPR121Array([0x5a, 0x5b, 0x5c, 0x5d])
+    mpr121array.start()
+    key_0 = Key("voice_key_1_position",0,0)
+    key_1 = Key("voice_key_2_position",0,1)
+    key_2 = Key("voice_key_3_position",1,1)
+    key_0.start()
+    time.sleep(5)
+    key_1.start()
+    time.sleep(5)
+    key_2.start()
 
