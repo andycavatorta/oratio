09b52387c0114b7b3599de6ced7476e8ea08f22e
Andy Cavatorta <andycavatorta@gmail.com>
2017-05-14 00:01:57 -0400

    preparing to communicate with drawbar sliders

--- Roles/avl_input3/drawbar_spi_test.py
+++ Roles/avl_input3/drawbar_spi_test.py
@@ -19,14 +19,14 @@ def spiRW (pin, msg):
 def read_channel (cs, ch):
   spiRW(cs, chr(ch << 4) + chr(0x00))
   val = spiRW(cs, chr(0x00) + chr(0x00))
-  print val
-  print ord(val[1][0])
-  print ord(val[1][1])
+  #print val
+  #print ord(val[1][0])
+  #print ord(val[1][1])
   return (ord(val[1][0]) << 2) | (ord(val[1][1]) >> 6)
 
 # quick way to get rid of outliers, make better later
 def read_avg (cs, ch, n=20):
-  return sum([read_channel(cs, ch) for i in xrange(n)]) / n
+  print sum([read_channel(cs, ch) for i in xrange(n)]) / n
 
 init()
 
