633c8708eebbb44e3c96576b378c34a8fcd14be2
Jesse T. Gonzalez <jtgonz1@gmail.com>
2017-06-16 13:15:32 -0400

    filt

--- Roles/avl_voice1/main.py
+++ Roles/avl_voice1/main.py
@@ -147,7 +147,7 @@ def network_message_handler(msg):
 
         # quick hack -- will make this better later!
         payload = eval(msg[1])
-        freq_1, gain, freq_2, vol_2, freq_3, vol_3, cutoff, pband = payload
+        freq_1, gain, freq_2, vol_2, freq_3, vol_3, cutoff_raw, pband = payload
 
         offset = 119104.6
         #print offset-int(payload[0])
@@ -164,13 +164,12 @@ def network_message_handler(msg):
         c.set_levels(1, level_2)
         c.set_levels(2, level_3)
 
-        # get period corresponding to cutoff frequency * 100 (in microseconds)
+        #get period corresponding to cutoff frequency * 100 (in microseconds)
 
-        # freq
-        # adj_period = 1e6 / (cutoff * freq1 * 100)
-        
-        # c.pport_write(1, adj_period)
-        # c.pband_size(pband/255)
+        cutoff_freq = (cutoff_raw - 0.5) * freq_1 + freq_1
+        adj_period = 1e6 / (cutoff_freq * 100)
+        c.pport_write(1, adj_period)
+        c.pband_size(pband/255)
 
         """
         if (payload[1] > 0.2):
