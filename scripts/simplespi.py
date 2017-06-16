61c04c5385e6064de770cca1e8f9bf9a214d0a96
Andy Cavatorta <andycavatorta@gmail.com>
2017-06-16 07:02:13 -0400

    spi testing

--- scripts/simplespi.py
+++ scripts/simplespi.py
@@ -15,7 +15,8 @@ CLK  = 18
 MISO = 23
 MOSI = 24
 CS   = 25
-mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+mcp1 = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+mcp2 = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
 # Hardware SPI configuration:
 # SPI_PORT   = 0
@@ -33,7 +34,16 @@ while True:
     values = [0]*8
     for i in range(8):
         # The read_adc function will get the value of the specified channel (0-7).
-        values[i] = mcp.read_adc(i)
+        values[i] = mcp1.read_adc(i)
+    # Print the ADC values.
+    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+    # Pause for half a second.
+    time.sleep(0.5)
+    # Read all the ADC channel values in a list.
+    values = [0]*8
+    for i in range(8):
+        # The read_adc function will get the value of the specified channel (0-7).
+        values[i] = mcp2.read_adc(i)
     # Print the ADC values.
     print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
     # Pause for half a second.
