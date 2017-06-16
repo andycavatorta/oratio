3062e62e38d8303e5101b63cd82a5945d03b58e5
Jesse T. Gonzalez <jtgonz1@gmail.com>
2017-06-16 16:39:50 -0400

    make voice 3 on/off

--- Roles/avl_voice3/main.py
+++ Roles/avl_voice3/main.py
@@ -149,35 +149,31 @@ def network_message_handler(msg):
         freq_1, gain, freq_2, vol_2, freq_3, vol_3, cutoff_raw, pband = payload
 
         offset = 167489
-        print payload
+        #print payload
 
         c.send_freq(0, offset-int(freq_1))
         c.send_freq(1, offset-int(freq_2))
         c.send_freq(2, offset-int(freq_3))
         #c.set_levels(0, 255 if payload[1] < 0.5 else int(180.0 * payload[1]))
-        c.set_levels(0, 0 if gain < 0.1 else int(240.0 * payload[1]))
-        c.set_levels(1, 0 if vol_2 < 0.1 else int(255.0 * vol_2))
-        c.set_levels(2, 0 if vol_3 < 0.1 else int(255.0 * vol_3))
 
-        cutoff_freq = (cutoff_raw - 0.5) * freq_1 + freq_1
-        adj_period = 1e6 / (cutoff_freq * 100)
-        c.pport_write(1, adj_period)
-        c.pband_size(pband/255)
-
-        """
-        if (payload[1] > 0.2):
-            c.send_freq(0, offset-int(freq_1))
-            c.send_freq(1, offset-int(freq_2))
-            c.send_freq(2, offset-int(freq_3))
-            #c.set_levels(0, 255 if payload[1] < 0.5 else int(180.0 * payload[1]))
-            c.set_levels(0, 0 if payload[1] < 0.1 else int(180.0 * payload[1]))
+        if (gain > 0.2):
+            c.set_levels(0, 0 if gain < 0.1 else int(240.0 * payload[1]))
             c.set_levels(1, 0 if vol_2 < 0.1 else int(255.0 * vol_2))
             c.set_levels(2, 0 if vol_3 < 0.1 else int(255.0 * vol_3))
         else:
             c.send_freq(0, 0)
             c.send_freq(1, 0)
             c.send_freq(2, 0)
-        """
+            c.set_levels(0, 0)
+            c.set_levels(1, 0)
+            c.set_levels(2, 0)
+
+
+        cutoff_freq = (cutoff_raw - 0.5) * freq_1 + freq_1
+        adj_period = 1e6 / (cutoff_freq * 100)
+        c.pport_write(1, adj_period)
+        c.pband_size(pband/255)
+
 network = None # makin' it global
 
 def init(HOSTNAME):
