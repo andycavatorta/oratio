6e9ad341f5abea56c4bca7c2f4856870685da096
Andy Cavatorta <andycavatorta@gmail.com>
2017-06-16 09:05:47 -0400

    copying new adc code to other layer pis

--- Roles/avl_layer3/main.py
+++ Roles/avl_layer3/main.py
@@ -1,38 +1,16 @@
-"""
-TASKS:
-    init:
-        maintain SSH tunnel to conductor
-        listen to Web API
-        open DB connection
-    runtime:
-        maintain DB of inventory histories?
-        generate HTML reports
-        serve reports on request
 
-API for conductor:
-    receive_inventory_and_map
-
-Dashboard:
-    web interface
-    websocket push
-    status of all camera_units
-        connected
-        current status ( color coded )
-        inventory
-        exceptions
-
-"""
-
-
-
-
+import Adafruit_MCP3008
 import importlib
 import json
 import os
+import Queue
+import random
+import RPi.GPIO as GPIO
 import settings 
 import sys
 import threading
 import time
+#import wiringpi as wpi
 
 from thirtybirds_2_0.Network.manager import init as network_init
 from thirtybirds_2_0.Network.email_simple import init as email_init
@@ -45,25 +23,139 @@ THIRTYBIRDS_PATH = "%s/thirtybirds" % (UPPER_PATH )
 sys.path.append(BASE_PATH)
 sys.path.append(UPPER_PATH)
 
-class Main(threading.Thread):
-    def __init__(self, hostname):
+
+class Drawbar():
+    def __init__(self, 
+        name, 
+        adc,
+        adc_channel,
+        detent_adc_values = [],
+        max = 1023,
+        min = 0
+        ):
+        self.name = name
+        self.adc = adc
+        self.adc_channel = adc_channel
+        self.detent_adc_values = detent_adc_values
+        self.max = max
+        self.min = min
+        self.previous_value = -1
+        self.change_threshold = 5
+
+    def detent_from_adc_value(self, raw_value):
+        return self.detent_adc_values.index(min(self.detent_adc_values, key=lambda x:abs(x-raw_value)))
+
+    def normalize_adc_value_to_min_max(self, val):
+        value_range = self.max - self.min
+        val_with_min_offset = val - self.min
+        value_normalized = float(val_with_min_offset) / float(value_range)
+        value_normalized = value_normalized if value_normalized > 0.0 else 0.0
+        value_normalized = value_normalized if value_normalized < 1.0 else 1.0
+        return 1.0 - value_normalized
+
+    def read(self):
+        # get raw value
+        raw_value = self.adc.read_adc(self.adc_channel)
+        # if close to last values, return -1
+        if abs(self.previous_value - raw_value) <= self.change_threshold:
+            self.previous_value = raw_value
+            return -1
+
+        self.previous_value = raw_value
+        # if detent values, get detents
+        if len(self.detent_adc_values) > 0:
+            return self.detent_from_adc_value(raw_value)
+        else:
+            return self.normalize_adc_value_to_min_max(raw_value)
+
+class Drawbars(threading.Thread):
+    def __init__(self):
         threading.Thread.__init__(self)
-        self.hostname = hostname
-        ### NETWORK ###
-
-
-        ### SET UP SUBSCRIPTIONS AND LISTENERS ###
-
-
-        ### SET UP ATTAR ### so any exceptions can be reported
-
+        CLK  = 18
+        MISO = 23
+        MOSI = 24
+        CS0   = 25
+        CS1   = 8
+        self.adc1 = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS0, miso=MISO, mosi=MOSI)
+        self.adc2 = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS1, miso=MISO, mosi=MOSI)
+        self.drawbars = [
+            Drawbar(
+                name="voice_3_db_harmonic" , 
+                detent_adc_values = [1022, 1008, 950, 888, 833, 760, 705, 640, 579],
+                adc=self.adc1,
+                adc_channel=0
+            ),
+            Drawbar(
+                name="voice_3_db_fine" , 
+                min=50, 
+                max=998,
+                adc=self.adc1,
+                adc_channel=1
+            ),
+            Drawbar(
+                name="voice_3_db_h1_fine" , 
+                min=60, 
+                max=950,
+                adc=self.adc1,
+                adc_channel=2
+            ),
+            Drawbar(
+                name="voice_3_db_h1_harmonic" , 
+                detent_adc_values = [1022, 1012, 972, 956, 844, 793, 733, 674, 582],
+                adc=self.adc1,
+                adc_channel=3
+            ),
+            Drawbar(
+                name="voice_3_db_h1_vol" , 
+                min=50, 
+                max=987,
+                adc=self.adc1,
+                adc_channel=4
+            ),
+            Drawbar(
+                name="voice_3_db_h2_fine" , 
+                min=54, 
+                max=1010,
+                adc=self.adc2,
+                adc_channel=0
+            ),
+            Drawbar(
+                name="voice_3_db_h2_harmonic" , 
+                detent_adc_values = [1021, 1011, 980, 932, 843, 806, 708,669, 594],
+                adc=self.adc2,
+                adc_channel=1
+            ),
+            Drawbar(
+                name="voice_3_db_h2_vol" , 
+                min=39, 
+                max=1008,
+                adc=self.adc2,
+                adc_channel=2
+            ),
+            Drawbar(
+                name="voice_3_db_filter_a" , 
+                min=55, 
+                max=1008,
+                adc=self.adc2,
+                adc_channel=3
+            ),
+            Drawbar(
+                name="voice_3_db_filter_b" , 
+                min=76, 
+                max=1009,
+                adc=self.adc2,
+                adc_channel=4
+            )
+        ]
 
     def run(self):
         while True:
-            time.sleep(60)
-
-        ###  ###
-
+            for drawbar in self.drawbars:
+                val = drawbar.read()
+                #print ">>>",drawbar.name, val
+                if val > -1:
+                    network.send(drawbar.name, val)
+                time.sleep(0.005)
 
 def network_status_handler(msg):
     print "network_status_handler", msg
@@ -76,9 +168,11 @@ def network_message_handler(msg):
         print "heartbeat received", msg
 
 network = None # makin' it global
+main = None
 
 def init(HOSTNAME):
     global network
+    global main
     network = network_init(
         hostname=HOSTNAME,
         role="client",
@@ -91,8 +185,6 @@ def init(HOSTNAME):
     )
 
     network.subscribe_to_topic("system")  # subscribe to all system messages
-    #network.subscribe_to_topic("sensor_data")  
-    main = Main(HOSTNAME)
-    main.daemon = True
-    main.start()
-
+    drawbars = Drawbars()
+    drawbars.daemon = True
+    drawbars.start()
