1fb492885e44ed93678d007f79af2d064f90ddcd
Andy Cavatorta <andycavatorta@gmail.com>
2017-06-17 18:58:18 -0400

    tuning

--- Roles/avl_voice2/main.py
+++ Roles/avl_voice2/main.py
@@ -51,6 +51,8 @@ global last_f1
 global last_f2
 global last_f3
 
+offset = 95945.1
+
 """
 class Main(threading.Thread):
     def __init__(self, hostname):
@@ -135,6 +137,7 @@ def network_status_handler(msg):
     print "network_status_handler", msg
 
 def network_message_handler(msg):
+    global offset 
     #print "network_message_handler", msg
     topic = msg[0]
     #host, sensor, data = yaml.safe_load(msg[1])
@@ -148,7 +151,7 @@ def network_message_handler(msg):
         payload = eval(msg[1])
         freq_1, gain, freq_2, vol_2, freq_3, vol_3, cutoff_raw, pband = payload
 
-        offset = 95925.1
+        
         print offset-int(payload[0])
 
 
