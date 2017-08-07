f17d1b01f48bf970b2691e509341608b7734e2d1
Andy Cavatorta <andycavatorta@gmail.com>
2017-08-07 17:31:50 -0400

    added stubs of files for formants

---
+++ Roles/avl-formant-1/main.py
@@ -0,0 +1,68 @@
+import os
+
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+UPPER_PATH = os.path.split(os.path.dirname(os.path.realpath(__file__)))[0]
+DEVICES_PATH = "%s/Hosts/" % (BASE_PATH )
+THIRTYBIRDS_PATH = "%s/thirtybirds_2_0" % (UPPER_PATH )
+
+sys.path.append(BASE_PATH)
+sys.path.append(UPPER_PATH)
+
+from thirtybirds_2_0.Network.manager import init as network_init
+
+class Network(object):
+    def __init__(self, hostname, network_message_handler, network_status_handler):
+        self.hostname = hostname
+        self.thirtybirds = network_init(
+            hostname=hostname,
+            role="client",
+            discovery_multicastGroup=settings.discovery_multicastGroup,
+            discovery_multicastPort=settings.discovery_multicastPort,
+            discovery_responsePort=settings.discovery_responsePort,
+            pubsub_pubPort=settings.pubsub_pubPort,
+            message_callback=network_message_handler,
+            status_callback=network_status_handler
+        )
+
+# Main handles network send/recv and can see all other classes directly
+class Main(threading.Thread):
+    def __init__(self, hostname):
+        threading.Thread.__init__(self)
+        self.network = Network(hostname, self.network_message_handler, self.network_status_handler)
+        self.queue = Queue.Queue()
+        self.network.thirtybirds.subscribe_to_topic("door_closed")
+
+    def network_message_handler(self, topic_msg):
+        # this method runs in the thread of the caller, not the tread of Main
+        topic, msg =  topic_msg # separating just to eval msg.  best to do it early.  it should be done in TB.
+        if len(msg) > 0: 
+            msg = eval(msg)
+        self.add_to_queue(topic, msg)
+
+    def network_status_handler(self, topic_msg):
+        # this method runs in the thread of the caller, not the tread of Main
+        print "Main.network_status_handler", topic_msg
+
+    def add_to_queue(self, topic, msg):
+        self.queue.put((topic, msg))
+
+    def run(self):
+        while True:
+            try:
+                topic, msg = self.queue.get(True)
+                if topic == "door_closed":
+                    door.add_to_queue(msg)
+                    self.network.thirtybirds.send("door_status", True)
+                    
+            except Exception as e:
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                print e, repr(traceback.format_exception(exc_type, exc_value,exc_traceback))
+
+
+def init(hostname):
+    main = Main(hostname)
+    main.daemon = True
+    main.start()
+    return main
+
+
