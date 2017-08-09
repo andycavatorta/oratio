9e3114925eb647feeab9e9f36d1ca7d387768e3d
Andy Cavatorta <andycavatorta@gmail.com>
2017-08-09 16:47:27 -0400

    adding mcp3008 test code

---
+++ Roles/avl-settings/mcp3008Adafruit_multi_test.py
@@ -0,0 +1,42 @@
+# Simple example of reading the MCP3008 analog input channels and printing
+# them all out.
+# Author: Tony DiCola
+# License: Public Domain
+import time
+
+# Import SPI library (for hardware SPI) and MCP3008 library.
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+
+# Software SPI configuration:
+CLK  = 18
+MISO = 23
+MOSI = 24
+CS   = 21
+
+for chip_select_pin in [21,22,23,24,25,26,27,28,29]:
+    test_adc(chip_select_pin)
+
+def test_adc(chip_select_pin):
+    mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=chip_select_pin, miso=MISO, mosi=MOSI)
+    print('Reading MCP3008 values, press Ctrl-C to quit...')
+    # Print nice channel column headers.
+    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
+    print('-' * 57)
+    # Main program loop.
+    for i in range(10) :
+        # Read all the ADC channel values in a list.
+        values = [0]*8
+        for i in range(8):
+            # The read_adc function will get the value of the specified channel (0-7).
+            values[i] = mcp.read_adc(i)
+        # Print the ADC values.
+        print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+        # Pause for half a second.
+        time.sleep(0.5)
+
+
+
+
+
