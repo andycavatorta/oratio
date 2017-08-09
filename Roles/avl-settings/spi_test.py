7eb3d09160d08e5dc293d83e23f6818bf8b4a8f7
Andy Cavatorta <andycavatorta@gmail.com>
2017-08-08 20:09:30 -0400

    adding SPI test code

--- Roles/avl-settings/spi_test.py
+++ Roles/avl-settings/spi_test.py
@@ -19,7 +19,7 @@ def spi_read_write(pin, msg):
 def read_channel(cs, ch):
     spi_read_write(cs, chr(ch << 4) + chr(0x00))
     val = spi_read_write(cs, chr(0x00) + chr(0x00))
-    print "--->", val
+    print "--->", cs, ch, val
     return (ord(val[1][0]) << 2) | (ord(val[1][1]) >> 6)
 
 for chip_select_pin in chip_select_pins:
