dea79ea14ffb53eceab5f31660c7f0b5c8eae2d8
Sam Tarakajian <starakajian@gmail.com>
2017-08-09 14:59:58 -0400

    Shifting gpio pins around again, since 20 and 21 are weird

--- Roles/avl-settings/pedal_test.py
+++ Roles/avl-settings/pedal_test.py
@@ -7,14 +7,16 @@ GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Forget pins 20 and 21. They seem to be a bit funny with the AudioInjector hat on
 
 while True:
     print "1w", GPIO.input(6)
     print "1b", GPIO.input(13)
     print "2w", GPIO.input(19)
     print "2b", GPIO.input(26)
-    print "3w", GPIO.input(16)
-    print "3b", GPIO.input(20)
+    print "3w", GPIO.input(12)
+    print "3b", GPIO.input(16)
     time.sleep(0.1)
