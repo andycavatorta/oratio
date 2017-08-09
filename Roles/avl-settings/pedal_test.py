3c83473126700e82a218d6485433800a2cab60ca
Andy Cavatorta <andycavatorta@gmail.com>
2017-08-09 11:50:29 -0400

    changing internal resistor polarity

--- Roles/avl-settings/pedal_test.py
+++ Roles/avl-settings/pedal_test.py
@@ -3,14 +3,14 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
     print "a", GPIO.input(6)
-    print "a", GPIO.input(13)
-    print "a", GPIO.input(19)
-    print "a", GPIO.input(26)
+    print "b", GPIO.input(13)
+    print "c", GPIO.input(19)
+    print "d", GPIO.input(26)
     time.sleep(0.1)
