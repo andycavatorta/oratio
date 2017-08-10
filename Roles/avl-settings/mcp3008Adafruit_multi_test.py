6d3db9319ca026a59537f92bd689b277114d18dc
Andy Cavatorta <andycavatorta@gmail.com>
2017-08-09 21:39:34 -0400

    minor fixes

--- Roles/avl-settings/mcp3008Adafruit_multi_test.py
+++ Roles/avl-settings/mcp3008Adafruit_multi_test.py
@@ -23,7 +23,7 @@ def test_adc(chip_select_pin):
     print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
     print('-' * 57)
     # Main program loop.
-    for i in range(1) :
+    for i in range(10) :
         # Read all the ADC channel values in a list.
         values = [0]*8
         for i in range(8):
