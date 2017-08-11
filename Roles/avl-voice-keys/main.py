cb53ea308d49629e82f53dcb985bdd2b29a93198
Dave Linnenbank <d.linnenbank@gmail.com>
2017-08-11 15:53:05 -0400

    Flipping staccato_1 and staccato_2 pin IDs

--- Roles/avl-voice-keys/main.py
+++ Roles/avl-voice-keys/main.py
@@ -114,8 +114,8 @@ class Main(threading.Thread):
         defs = [
             ("hold", 26),
             ("staccato_3", 19),
-            ("staccato_2", 13),
-            ("staccato_1", 06),
+            ("staccato_2", 06),
+            ("staccato_1", 13),
         ]
         self.buttons = Buttons(defs)
         self.hold = False
