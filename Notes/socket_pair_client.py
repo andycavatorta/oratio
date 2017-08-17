4c7e3c3e6838dd9f6f424acdf12b9b6cf7f338bf
Andy Cavatorta <andycavatorta@gmail.com>
2017-08-17 15:17:35 -0400

    adding socket code for connection to Koby

--- Notes/socket_pair_client.py
+++ Notes/socket_pair_client.py
@@ -15,10 +15,10 @@ context = zmq.Context()
 socket = context.socket(zmq.SUB)
 
 print "Collecting updates from weather server..."
-socket.connect ("tcp://localhost:%s" % port)
+socket.connect ("tcp://192.168.1.40:%s" % port)
 
-if len(sys.argv) > 2:
-    socket.connect ("tcp://localhost:%s" % port1)
+#if len(sys.argv) > 2:
+#    socket.connect ("tcp://localhost:%s" % port1)
 # Subscribe to zipcode, default is NYC, 10001
 topicfilter = "10001"
 socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
