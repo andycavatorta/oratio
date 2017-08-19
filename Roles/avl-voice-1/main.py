44da10c3a5a2aeb396f54467833dbe46829dcba7
Andy Cavatorta <andycavatorta@gmail.com>
2017-08-18 21:58:52 -0400

    fidging freq numbers to adjust for flatness on voice 1

--- Roles/avl-voice-1/main.py
+++ Roles/avl-voice-1/main.py
@@ -139,18 +139,19 @@ class Main(threading.Thread):
                         params.append(param)
                     freq_root, vol, freq_sub1, vol_sub1, freq_sub2, vol_sub2 = params
                     # update intermediate frequency if new data is available
+                    measure_xtal_freq = crystal.measure_xtal_freq()
                     self.xtal_freq = crystal.measure_xtal_freq() or self.xtal_freq
-                    print params, self.xtal_freq
+                    print params, self.xtal_freq, measure_xtal_freq
 
                     # subvoice 1 (fundamental) frequency
-                    crystal.set_freq(0, vol and (self.xtal_freq - (freq_root + self.f_offset)))
+                    crystal.set_freq(0, vol and (self.xtal_freq - (freq_root+1 + self.f_offset)))
 
                     # subvoice 2 frequency and volume
-                    crystal.set_freq(1, vol_sub1 and (self.xtal_freq - (freq_sub1 + self.f_offset)))
+                    crystal.set_freq(1, vol_sub1 and (self.xtal_freq - (freq_sub1+1 + self.f_offset)))
                     crystal.set_volume(1, map_subvoice_volume(vol_sub1))
 
                     # subvoice 3 frequency and volume
-                    crystal.set_freq(2, vol_sub2 and (self.xtal_freq - (freq_sub2 + self.f_offset)))
+                    crystal.set_freq(2, vol_sub2 and (self.xtal_freq - (freq_sub2+1 + self.f_offset)))
                     crystal.set_volume(2, map_subvoice_volume(vol_sub2))
 
                     #crystal.set_freq(0, self.xtal_freq - (freq_root + self.f_offset))
