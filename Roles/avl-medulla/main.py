91da0ba2bc82b0e52786b25608fbf3db1ba99ede
Andy Cavatorta <andycavatorta@gmail.com>
2017-09-21 18:48:21 -0400

    adding medulla

---
+++ Roles/avl-medulla/main.py
@@ -0,0 +1,139 @@
+import commands
+import os
+import Queue
+import settings
+import time
+import threading
+import wiringpi as wpi
+import sys
+import traceback
+
+#BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+#UPPER_PATH = os.path.split(os.path.dirname(os.path.realpath(__file__)))[0]
+#DEVICES_PATH = "%s/Hosts/" % (BASE_PATH )
+#THIRTYBIRDS_PATH = "%s/thirtybirds_2_0" % (UPPER_PATH )
+
+#sys.path.append(BASE_PATH)
+#sys.path.append(UPPER_PATH)
+
+from thirtybirds_2_0.Network.manager import init as network_init
+from thirtybirds_2_0.Updates.manager import init as updates_init
+
+#wpi.wiringPiSetup()
+#wpi.wiringPiSPISetup(0, 500000)
+
+class Network(object):
+    def __init__(self, hostname, network_message_handler, network_status_handler):
+        self.hostname = hostname
+        self.thirtybirds = network_init(
+            hostname=hostname,
+            role="client",
+            discovery_multicastGroup=settings.discovery_multicastGroup,
+            discovery_multicastPort=settings.discovery_multicastPort,
+            discovery_responsePort=settings.discovery_responsePort,
+            pubsub_pubPort=settings.pubsub_pubPort,
+            message_callback=network_message_handler,
+            status_callback=network_status_handler
+        )
+
+
+########################
+## UTILS
+########################
+
+class Utils(object):
+    def __init__(self, hostname):
+        self.hostname = hostname
+    def reboot(self):
+        os.system("sudo reboot now")
+
+    def remote_update_git(self, oratio, thirtybirds, update, upgrade):
+        if oratio:
+            subprocess.call(['sudo', 'git', 'pull'], cwd='/home/pi/oratio')
+        if thirtybirds:
+            subprocess.call(['sudo', 'git', 'pull'], cwd='/home/pi/thirtybirds_2_0')
+        return 
+
+    def remote_update_scripts(self):
+        updates_init("/home/pi/oratio", False, True)
+        return
+
+    def get_update_script_version(self):
+        (updates, ghStatus, bsStatus) = updates_init("/home/pi/oratio", False, False)
+        return updates.read_version_pickle()
+
+    def get_git_timestamp(self):
+        return commands.getstatusoutput("cd /home/pi/oratio/; git log -1 --format=%cd")[1]   
+
+    def get_temp(self):
+        return commands.getstatusoutput("/opt/vc/bin/vcgencmd measure_temp")[1]
+
+    def get_cpu(self):
+        bash_output = commands.getstatusoutput("uptime")[1]
+        split_output = bash_output.split(" ")
+        return split_output[12]
+
+    def get_uptime(self):
+        bash_output = commands.getstatusoutput("uptime")[1]
+        split_output = bash_output.split(" ")
+        return split_output[4]
+
+    def get_disk(self):
+        # stub for now
+        return "0"
+
+    def get_client_status(self):
+        return (self.hostname, self.get_update_script_version(), self.get_git_timestamp(), self.get_temp(), self.get_cpu(), self.get_uptime(), self.get_disk())
+
+# Main handles network send/recv and can see all other classes directly
+class Main(threading.Thread):
+    def __init__(self, hostname):
+        threading.Thread.__init__(self)
+        self.network = Network(hostname, self.network_message_handler, self.network_status_handler)
+        self.queue = Queue.Queue()
+        self.last_master_volume_level = 0
+        self.utils = Utils(hostname)
+        self.network.thirtybirds.subscribe_to_topic("voice_1")
+        self.network.thirtybirds.subscribe_to_topic("client_monitor_request")
+
+    def network_message_handler(self, topic_msg):
+        # this method runs in the thread of the caller, not the tread of Main
+        topic, msg =  topic_msg # separating just to eval msg.  best to do it early.  it should be done in TB.
+        if len(msg) > 0: 
+            msg = eval(msg)
+        self.add_to_queue(topic, msg)
+
+    def network_status_handler(self, topic_msg):
+        # this method runs in the thread of the caller, not the tread of Main
+        print "Main.network_status_handler", topic_msg
+
+    def add_to_queue(self, topic, msg):
+        self.queue.put((topic, msg))
+
+    def run(self):
+        while True:
+            try:
+                topic, msg = self.queue.get(True)
+                if topic == "client_monitor_request":
+                    self.network.thirtybirds.send("client_monitor_response", self.utils.get_client_status())
+
+                if topic == "voice_1":
+                    master_volume = msg[1]
+                    master_volume = 0 if master_volume < 0.1 else master_volume
+                    if master_volume != self.last_master_volume_level :
+                        gain = int(120 + master_volume * 40) if master_volume > 0.1 else 0
+                        print "master_volume", master_volume, "gain", gain
+                        wpi.wiringPiSPIDataRW(0, chr(gain) + chr(0))
+                        self.last_master_volume_level = master_volume
+                time.sleep(0.01)
+            except Exception as e:
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                print e, repr(traceback.format_exception(exc_type, exc_value,exc_traceback))
+
+
+def init(hostname):
+    main = Main(hostname)
+    main.daemon = True
+    main.start()
+    return main
+
