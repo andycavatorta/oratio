cc2dc898de69db9d4fa0407c768b305848e81ff8
Jesse T. Gonzalez <jtgonz1@gmail.com>
2017-10-03 17:03:09 +0200

    switch stat keys

--- Roles/avl-voice-keys/main.py
+++ Roles/avl-voice-keys/main.py
@@ -1,4 +1,4 @@
-from __future__ import division
+    from __future__ import division
 
 import test_that_works
 import AMT203
@@ -160,8 +160,8 @@ class Main(threading.Thread):
         defs = [
             ("hold", 26),
             ("staccato_3", 19),
-            ("staccato_2", 06),
-            ("staccato_1", 13),
+            ("staccato_2", 13),
+            ("staccato_1", 06),
         ]
         self.buttons = Buttons(defs)
         self.hold = False
