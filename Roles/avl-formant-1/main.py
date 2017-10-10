06b41ff4a1d6bf38670bdfe55c3cc955edc5ffac
Andy Cavatorta <andycavatorta@gmail.com>
2017-10-10 13:19:12 +0200

    tweaking gain contour

--- Roles/avl-formant-1/main.py
+++ Roles/avl-formant-1/main.py
@@ -129,7 +129,7 @@ class Main(threading.Thread):
 
                 if topic == "voice_1":
                     master_volume = msg[1]
-                    master_volume = 0 if master_volume < 0.1 else master_volume
+                    master_volume = 0 if master_volume < 0.1 else master_volume - 0.1
                     if master_volume != self.last_master_volume_level :
                         gain = int(120 + master_volume * 55) if master_volume > 0.1 else 0
                         print "master_volume", master_volume, "gain", gain
