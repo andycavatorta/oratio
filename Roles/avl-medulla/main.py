063300fe1a40366d79cdb3d54e8975f13b6cde78
Andy Cavatorta <andycavatorta@gmail.com>
2019-08-20 21:26:43 +0200

    typo

--- Roles/avl-medulla/main.py
+++ Roles/avl-medulla/main.py
@@ -146,8 +146,8 @@ class Main(threading.Thread):
     def network_message_handler(self, topic_msg):
         # this method runs in the thread of the caller, not the tread of Main
         topic, msg =  topic_msg # separating just to eval msg.  best to do it early.  it should be done in TB.
-        if len(msg) > 0: 
-            msg = eval(msg)
+        #if len(msg) > 0: 
+        #    msg = eval(msg)
         self.add_to_queue(topic, msg)
 
     def network_status_handler(self, topic_msg):
