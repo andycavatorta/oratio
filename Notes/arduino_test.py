a32a710ef39997e9e76d54bb5a9a698b6790a62c
Andy Cavatorta <andycavatorta@gmail.com>
2019-08-22 17:50:59 +0200

    testing serial comms through dev file

--- Notes/arduino_test.py
+++ Notes/arduino_test.py
@@ -6,10 +6,10 @@ for id in range(47):
     id_str = "{}\n".format(5000+id)
     rand_int = random.randint(0,4000)
     rand_str = "{}\n".format(rand_int)
-    print id_str, rand_int
-    time.sleep(0.05)
+    print repr(id_str), repr(rand_str)
+    time.sleep(1)
     arduino_connection.write(id_str)
-    time.sleep(0.05)
+    time.sleep(1)
     arduino_connection.write(rand_str)
 
 arduino_connection.close()
