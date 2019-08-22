26894eb3ecde91cb5ec8c7a23efd58836ec53d98
Andy Cavatorta <andycavatorta@gmail.com>
2019-08-22 17:56:02 +0200

    testing serial comms through dev file

--- Notes/arduino_test.py
+++ Notes/arduino_test.py
@@ -4,12 +4,12 @@ import time
 arduino_connection = open("/dev/ttyACM0",'w')
 for id in range(47):
     id_str = "{}\n".format(5000+id)
-    rand_int = random.randint(0,4000)
+    rand_int = random.randint(0,1000)
     rand_str = "{}\n".format(rand_int)
     print repr(id_str), repr(rand_str)
-    time.sleep(0.5)
+    time.sleep(0.75)
     arduino_connection.write(id_str)
-    time.sleep(0.5)
+    time.sleep(0.75)
     arduino_connection.write(rand_str)
 
 arduino_connection.close()
