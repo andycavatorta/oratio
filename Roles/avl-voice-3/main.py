26bc4103feecc0de10b3ec8bc52932981ac45d72
Andy Cavatorta <andycavatorta@gmail.com>
2019-08-24 23:41:27 +0200

    adding mandala tests to voice-3

--- Roles/avl-voice-3/main.py
+++ Roles/avl-voice-3/main.py
@@ -31,13 +31,14 @@ pin info:
 """
 
 import commands
+import crystal_helpers as crystal
 import os
+import Queue
 import sys
 import settings
+import time
 import traceback
 import threading
-import Queue
-import crystal_helpers as crystal
 
 BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 UPPER_PATH = os.path.split(os.path.dirname(os.path.realpath(__file__)))[0]
@@ -49,6 +50,7 @@ sys.path.append(UPPER_PATH)
 
 from thirtybirds_2_0.Network.manager import init as network_init
 from thirtybirds_2_0.Updates.manager import init as updates_init
+
 
 class Network(object):
     def __init__(self, hostname, network_message_handler, network_status_handler):
@@ -100,7 +102,7 @@ class Utils(object):
         return updates.read_version_pickle()
 
     def get_git_timestamp(self):
-        return commands.getstatusoutput("cd /home/pi/oratio/; git log -1 --format=%cd")[1]   
+        return commands.getstatusoutput("cd /home/pi/oratio/; git log -1 --format=%cd")[1]
 
     def get_temp(self):
         return commands.getstatusoutput("/opt/vc/bin/vcgencmd measure_temp")[1]
@@ -123,6 +125,7 @@ class Utils(object):
         return (self.hostname, self.get_update_script_version(), self.get_git_timestamp(), self.get_temp(), self.get_cpu(), self.get_uptime(), self.get_disk())
 
 
+
 # Main handles network send/recv and can see all other classes directly
 class Main(threading.Thread):
     def __init__(self, hostname):
@@ -130,14 +133,33 @@ class Main(threading.Thread):
         self.network = Network(hostname, self.network_message_handler, self.network_status_handler)
         self.queue = Queue.Queue()
         self.utils = Utils(hostname)
-
+        #self.gainRampThread = gainRampThread
         # default intermediate frequency
+        #self.xtal_freq = 167233.6
         self.xtal_freq = 96131.8
         self.f_offset = 0           # adjust output freq
-
+        self.status = {
+            "avl-voice-3":"pass", # because this passes if it can respond.  maybe better tests in future
+            "avl-voice-3-crystal-frequency-counter":"unset",
+            "avl-voice-3-harmonic-generators":"unset",
+            "avl-voice-3-harmonic-volume":"unset",
+        }
         # get voice messages
         self.network.thirtybirds.subscribe_to_topic("voice_3")
         self.network.thirtybirds.subscribe_to_topic("client_monitor_request")
+        self.network.thirtybirds.subscribe_to_topic("mandala_device_request")
+
+    def update_device_status(self, devicename, status):
+        print "update_device_status",devicename, status
+        if self.status[devicename] != status:
+            self.status[devicename] = status
+            msg = [devicename, status]
+            self.network.thirtybirds.send("mandala_device_status", msg)
+
+    def get_device_status(self, devicename, status):
+        for devicename in self.status:
+            msg = [devicename, self.status[devicename]]
+            self.network.thirtybirds.send("mandala_device_status", msg)
 
     def network_message_handler(self, topic_msg):
         # this method runs in the thread of the caller, not the tread of Main
@@ -169,41 +191,50 @@ class Main(threading.Thread):
                         param = 0 if msg[5] < thresh[2] and i in (4,5) else param     # subvoice 2
                         params.append(param)
                     freq_root, vol, freq_sub1, vol_sub1, freq_sub2, vol_sub2 = params
-                    vol = max(vol-0.13, 0) 
-                    vol_sub1 = max(vol_sub1-0.09, 0) 
-                    vol_sub2 = max(vol_sub2-0.09, 0) 
-                    print "volume=", vol, vol_sub1, vol_sub2
-                    # update intermediate frequency if new data is available
-                    #measure_xtal_freq = crystal.measure_xtal_freq()
-                    self.xtal_freq = crystal.measure_xtal_freq() or self.xtal_freq
-                    print "adjusted:", params, self.xtal_freq
 
-                    # subvoice 1 (fundamental) frequency
-                    crystal.set_freq(0, vol and (self.xtal_freq - (freq_root + self.f_offset)))
+                    #`vol = max(vol-0.03, 0) 
 
-                    # subvoice 2 frequency and volume
-                    crystal.set_freq(1, vol_sub1 and (self.xtal_freq - (freq_sub1 + self.f_offset)))
-                    crystal.set_volume(1, map_subvoice_volume(vol_sub1))
+                    try:
+                        self.xtal_freq = crystal.measure_xtal_freq() or self.xtal_freq
+                        self.update_device_status("avl-voice-3-crystal-frequency-counter", "pass")
+                    except Exception as e:
+                        self.update_device_status("avl-voice-3-crystal-frequency-counter", "fail")
 
-                    # subvoice 3 frequency and volume
-                    crystal.set_freq(2, vol_sub2 and (self.xtal_freq - (freq_sub2 + self.f_offset)))
-                    crystal.set_volume(2, map_subvoice_volume(vol_sub2))
+                    try:
+                        # subvoice 1 (fundamental) frequency
+                        crystal.set_freq(0, vol and (self.xtal_freq - (freq_root + self.f_offset)))
+                        # subvoice 2 frequency 
+                        crystal.set_freq(1, vol_sub1 and (self.xtal_freq - (freq_sub1 + self.f_offset)))
+                        # subvoice 3 frequency 
+                        crystal.set_freq(2, vol_sub2 and (self.xtal_freq - (freq_sub2 + self.f_offset)))
+                        self.update_device_status("avl-voice-3-harmonic-generators", "pass")
+                    except Exception as e:
+                        self.update_device_status("avl-voice-3-harmonic-generators", "fail")
+
+                    try:
+                        # subvoice 2 volume
+                        crystal.set_volume(1, map_subvoice_volume(vol_sub1))
+                        # subvoice 3 volume
+                        crystal.set_volume(2, map_subvoice_volume(vol_sub2))
+                        self.update_device_status("avl-voice-3-harmonic-volume", "pass")
+                    except Exception as e:
+                        self.update_device_status("avl-voice-3-harmonic-volume", "fail")
+
+
 
                 if topic == "client_monitor_request":
                     self.network.thirtybirds.send("client_monitor_response", self.utils.get_client_status())
 
-            
+                if topic == "mandala_device_request":
+                    self.get_device_status()
+
             except Exception as e:
                 exc_type, exc_value, exc_traceback = sys.exc_info()
                 print e, repr(traceback.format_exception(exc_type, exc_value,exc_traceback))
 
-mainloop = False
-
 def init(hostname):
-    global mainloop
     crystal.init()
-
-    mainloop= main = Main(hostname)
+    main = Main(hostname)
     main.daemon = True
     main.start()
     return main
