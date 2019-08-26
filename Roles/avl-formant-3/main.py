0cba78ded6d49900730f4f56033a37b354a5bddb
Andy Cavatorta <andycavatorta@gmail.com>
2019-08-26 22:30:41 +0200

    ramps to test source of crackling

--- Roles/avl-formant-3/main.py
+++ Roles/avl-formant-3/main.py
@@ -156,19 +156,20 @@ class Main(threading.Thread):
                         self.get_device_status()
                     if topic == "voice_3":
                         master_volume = msg[1]
+                        master_volume = 0 if master_volume < 0.1 else master_volume - 0.1
                 except Queue.Empty:
                     pass
-
-                master_volume = 0 if master_volume < 0.1 else master_volume - 0.1
                 #if master_volume != self.last_master_volume_level :
                 if master_volume > self.last_master_volume_level :
                     self.last_master_volume_level = self.last_master_volume_level + 0.01
+                    gain = int(100 + (100 * self.last_master_volume_level)) if self.last_master_volume_level > 0.01 else 0
+                    print "master_volume=", master_volume, " gain=", gain,  "self.last_master_volume_level", self.last_master_volume_level
+                    wpi.wiringPiSPIDataRW(0, chr(gain) + chr(0))
                 if master_volume < self.last_master_volume_level :
                     self.last_master_volume_level = self.last_master_volume_level - 0.01
-                gain = int(100 + (100 * self.last_master_volume_level)) if self.last_master_volume_level > 0.01 else 0
-                #gain = int(100 + (100 * master_volume)) if master_volume > 0.01 else 0
-                print "master_volume=", master_volume, " gain=", gain,  "self.last_master_volume_level", self.last_master_volume_level
-                wpi.wiringPiSPIDataRW(0, chr(gain) + chr(0))
+                    gain = int(100 + (100 * self.last_master_volume_level)) if self.last_master_volume_level > 0.01 else 0
+                    print "master_volume=", master_volume, " gain=", gain,  "self.last_master_volume_level", self.last_master_volume_level
+                    wpi.wiringPiSPIDataRW(0, chr(gain) + chr(0))
                 time.sleep(0.01)
             except Exception as e:
                 exc_type, exc_value, exc_traceback = sys.exc_info()
