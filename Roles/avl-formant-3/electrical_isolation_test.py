a6556ee2e1e42e3b82ffc8f6c0c30699164b353d
Andy Cavatorta <andycavatorta@gmail.com>
2019-08-26 16:50:23 +0200

    adding test code

--- Roles/avl-formant-3/electrical_isolation_test.py
+++ Roles/avl-formant-3/electrical_isolation_test.py
@@ -5,11 +5,11 @@ wpi.wiringPiSetup()
 wpi.wiringPiSPISetup(0, 500000)
 
 while True:
-    for gain in range(255):
+    for gain in range(192):
         print gain
         wpi.wiringPiSPIDataRW(0, chr(gain) + chr(0))
-        time.sleep(0.05)
-    for ungain in range(255):
-        print 255-ungain
-        wpi.wiringPiSPIDataRW(0, chr(255-ungain) + chr(0))
-        time.sleep(0.05)
+        time.sleep(0.02)
+    for ungain in range(192):
+        print 192-ungain
+        wpi.wiringPiSPIDataRW(0, chr(192-ungain) + chr(0))
+        time.sleep(0.02)
