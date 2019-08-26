6452995c092ba67c9637a3b3819459df643325a8
Andy Cavatorta <andycavatorta@gmail.com>
2019-08-26 21:30:13 +0200

    ramps to test source of crackling

--- Roles/avl-formant-3/electrical_isolation_test.py
+++ Roles/avl-formant-3/electrical_isolation_test.py
@@ -12,12 +12,12 @@ while True:
 """
 while True:
     for gain in range(92):
-        gain_offset = gain + 100 + random.randint(0,10)
+        gain_offset = gain + 100 + random.randint(0,20)
         print gain_offset
         wpi.wiringPiSPIDataRW(0, chr(gain_offset) + chr(0))
-        time.sleep(0.01)
+        time.sleep(0.02)
     for ungain in range(92):
-        gain_offset = 192-ungain + random.randint(0,10)
+        gain_offset = 192-ungain + random.randint(0,20)
         print gain_offset
         wpi.wiringPiSPIDataRW(0, chr(gain_offset) + chr(0))
-        time.sleep(0.01)
+        time.sleep(0.02)
