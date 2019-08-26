9505465d75e36e370b804c3660f6ee2a5dc9a1e5
Andy Cavatorta <andycavatorta@gmail.com>
2019-08-26 21:27:14 +0200

    debugging catastrophic failure of everything.  looking for source

--- Roles/avl-formant-3/electrical_isolation_test.py
+++ Roles/avl-formant-3/electrical_isolation_test.py
@@ -14,9 +14,9 @@ while True:
         gain_offset = gain + 100
         print gain_offset
         wpi.wiringPiSPIDataRW(0, chr(gain_offset) + chr(0))
-        time.sleep(0.1)
+        time.sleep(0.01)
     for ungain in range(92):
         gain_offset = 192-ungain
         print gain_offset
         wpi.wiringPiSPIDataRW(0, chr(gain_offset) + chr(0))
-        time.sleep(0.1)
+        time.sleep(0.01)
