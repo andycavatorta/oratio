29e18384bea8267078f336d68a2f1d277583319f
Andy Cavatorta <andycavatorta@gmail.com>
2019-08-27 16:35:35 +0200

    adjusting volume ramping to reduce noice

--- Roles/avl-formant-3/electrical_isolation_test.py
+++ Roles/avl-formant-3/electrical_isolation_test.py
@@ -14,10 +14,10 @@ while True:
     for gain in range(92):
         gain_offset = (gain*1) + 100 
         print gain_offset
-        wpi.wiringPiSPIDataRW(0, chr(gain_offset) + chr(0))
+        #wpi.wiringPiSPIDataRW(0, chr(gain_offset) + chr(0))
         time.sleep(0.001)
     for ungain in range(92):
         gain_offset = 192-(ungain*1) 
         print gain_offset
-        wpi.wiringPiSPIDataRW(0, chr(gain_offset) + chr(0))
+        #wpi.wiringPiSPIDataRW(0, chr(gain_offset) + chr(0))
         time.sleep(0.001)
